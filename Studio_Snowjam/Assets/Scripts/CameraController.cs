c1f3b002ca1b1493386386814f3b89ef08cbefd1
Channa188 <clea_h@p-hall.net>
2021-12-16 15:41:53 -0500

    2.0

---
+++ Studio_Snowjam/Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    GameObject player;
+    Transform pos;
+
+
+    void Start()
+    {
+        player = GameObject.Find("Toasty");
+        pos = player.GetComponent<Transform>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 position = pos.position;
+        gameObject.transform.position = new Vector3(position.x, position.y, gameObject.transform.position.z);
+    }
+}
