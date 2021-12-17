3b5bf2bde29019344068dc0584aaf72934e3e344
anvi-pen <91691176+anvi-pen@users.noreply.github.com>
2021-12-16 18:37:01 -0800

    Modifications to Enemy Projectiles

--- Studio_Snowjam/Assets/Scripts (Anvi)/EnemyProjectileMovement.cs
+++ Studio_Snowjam/Assets/Scripts (Anvi)/EnemyProjectileMovement.cs
@@ -6,7 +6,7 @@ public class EnemyProjectileMovement : MonoBehaviour
 {
     float lifetime = 0f;
     [SerializeField] float maxLifetime = 7f;
-    [SerializeField] float speed = 5f;
+    [SerializeField] float speed = 8f;
 
     GameObject player;
     Vector2 target;
@@ -37,11 +37,18 @@ public class EnemyProjectileMovement : MonoBehaviour
         float step = speed * Time.deltaTime;
         transform.position = Vector2.MoveTowards(transform.position, target, step);
 
+        if ((transform.position.x == target.x) && (transform.position.y == target.y))
+        {
+            Destroy(gameObject);
+        }
+
+        // destroy projectile after exceeding lifetime (if it did not hit anything)
         lifetime += Time.deltaTime;
         if (lifetime >= maxLifetime)
         {
             Destroy(gameObject);
         }
+        
     }
 
     void OnCollisionEnter2D(Collision2D collision)
