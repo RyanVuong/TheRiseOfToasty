3749204f1b89c1f4f403315bbfce08118c2b573d
Channa188 <clea_h@p-hall.net>
2021-12-16 21:00:08 -0500

    6.0

--- Studio_Snowjam/Assets/Clea Scripts/powerUps.cs
+++ Studio_Snowjam/Assets/Clea Scripts/powerUps.cs
@@ -7,24 +7,25 @@ public class powerUps : MonoBehaviour
     public float multiplier = 1.4f;
     public GameObject pickupEffect;
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Player"))
+    public Health health;
+
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    { 
+        if (collision.tag == "PowerUp")
         {
-            Pickup();
-        }
+            Destroy(collision.gameObject);
+            health.DamagePlayer(-1);
+         }
     }
 
-    void Pickup(Collider player)
+    public void setAttack()
     {
-        //Spawn pick up effect
-        Instantiate(pickupEffect, transform.position, transform.rotation);
+        ref int CH = ref health.CurrentHealth();
+        ref int attack += CH - health.MaxHealth();
 
-        //Apply power up to player
-        playerStats stats = player.GetComponent<playerStats>();
-        stats.health *= multiplier;
-
-        //Remove power up object
-        Destroy(gameObject);
     }
+
 }
+
+
